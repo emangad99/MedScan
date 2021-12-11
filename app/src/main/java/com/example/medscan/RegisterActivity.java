8ec813672706c6fc4fcfe5a81ac63bd62e79a280
fathy <https://github.com/fathytarek>
2021-12-11 09:52:18 +0200

    medscan update1

---
+++ app/src/main/java/com/example/medscan/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.medscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
