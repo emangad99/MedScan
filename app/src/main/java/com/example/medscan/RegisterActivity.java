2ab7dea6e6f5960cd73f87d8635243b4af7ba3c3
nada <nadaayman2110>
2021-12-12 16:32:34 +0200

    RegisterActivity.java

--- app/src/main/java/com/example/medscan/RegisterActivity.java
+++ app/src/main/java/com/example/medscan/RegisterActivity.java
@@ -1,16 +1,31 @@
 package com.example.medscan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-TextView signin;
-Button signup;
+    EditText txtFirstName ,txtLastName , txtEmail ,txtPass;
+    ImageView btnGoogle,btnFacebook,btnTwitter;
+    String pw = "^.*(?=.{8,})(?=.*\\d)(?=.*[a-zA-Z])|(?=.{8,})(?=.*\\d)(?=.*[!@#$%^&])|(?=.{8,})(?=.*[a-zA-Z])(?=.*[!@#$%^&]).*$";
+    FirebaseAuth fAuth;
+    TextView signin;
+    Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +33,61 @@ Button signup;
 
         signin=findViewById(R.id.Sign_in);
         signup=findViewById(R.id.btn_signup);
+        txtFirstName=findViewById(R.id.txt_fname);
+        txtLastName = findViewById(R.id.txt_lname);
+        txtEmail = findViewById(R.id.Email);
+        txtPass = findViewById(R.id.Password);
+        fAuth = FirebaseAuth.getInstance();
+        btnFacebook = findViewById(R.id.Facebook);
+        btnGoogle = findViewById(R.id.Google);
+        btnTwitter = findViewById(R.id.Twitter);
+        if (fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            finish();
+        }
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent h=new Intent(RegisterActivity.this,HomeActivity.class);
-                startActivity(h);
+
+
+                String firstname = txtFirstName.getText().toString();
+                String lastname = txtLastName.getText().toString();
+                String email = txtEmail.getText().toString();
+                String password = txtPass.getText().toString();
+
+                if (firstname.isEmpty()) {
+                    txtFirstName.setError("First Name is required");
+                    txtFirstName.requestFocus();
+
+                }
+                if (lastname.isEmpty()) {
+                    txtLastName.setError("Last Name is required");
+                    txtLastName.requestFocus();
+                }
+                if (email.isEmpty()) {
+                    txtEmail.setError("Email is required");
+                    txtEmail.requestFocus();
+                }
+                if (!password.matches(pw)) {
+                    txtPass.setError("Password must contain at least 8 and must contain Upper case, Lower case,Numbers and signs");
+                    txtPass.requestFocus();
+                }
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"User created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+                        }else {
+                            Toast.makeText(RegisterActivity.this,"Error" +task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
             }
         });
 
