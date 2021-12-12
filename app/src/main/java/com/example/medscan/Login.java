84bb96b9cd14d9bd597956f77e59160cc42eaf74
nada <nadaayman2110>
2021-12-12 18:13:03 +0200

    Login.java

--- app/src/main/java/com/example/medscan/Login.java
+++ app/src/main/java/com/example/medscan/Login.java
@@ -1,14 +1,20 @@
 package com.example.medscan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -16,6 +22,7 @@ public class Login extends AppCompatActivity {
     TextView forgotPassword,signup;
     Button login;
     FirebaseAuth fAuth;
+    String pw = "^.*(?=.{8,})(?=.*\\d)(?=.*[a-zA-Z])|(?=.{8,})(?=.*\\d)(?=.*[!@#$%^&])|(?=.{8,})(?=.*[a-zA-Z])(?=.*[!@#$%^&]).*$";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +32,7 @@ public class Login extends AppCompatActivity {
         login=findViewById(R.id.btn_login3);
         mEmail = findViewById(R.id.txt_email);
         mPassword = findViewById(R.id.txt_password);
+        fAuth= FirebaseAuth.getInstance();
 
 
 
@@ -47,9 +55,39 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent login=new Intent(Login.this,HomeActivity.class);
-                startActivity(login);
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.isEmpty()) {
+                    mPassword.setError("Last Password is required");
+                    mPassword.requestFocus();
+                }
+                if (!password.matches(pw)) {
+                    mPassword.setError("Password must contain at least 8 and must contain Upper case, Lower case,Numbers and signs ");
+                    mPassword.requestFocus();
+                }
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Logged in Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+
+                        }else {
+                            Toast.makeText(Login.this,"Error" +task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
             }
