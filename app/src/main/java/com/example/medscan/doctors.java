7b67f0e640f5a1fc2fecf6bf70a3fb5d3df24084
eman_gad <emangad3110@gmail.com>
2022-02-08 20:20:18 +0200

    init commit

--- app/src/main/java/com/example/medscan/doctors.java
+++ app/src/main/java/com/example/medscan/doctors.java
@@ -15,7 +15,8 @@ public class doctors extends AppCompatActivity {
 
     private RelativeLayout mlayout ,mlayout2 ,mlayout3 ,mlayout4 ,mlayout5;
     private ImageView doc1 , doc2 , doc3 , doc4 , doc5;
-    private TextView namdoc1, namdoc2;
+    private TextView namdoc1, namdoc2 ,namdoc3 , namdoc4 ,namdoc5;
+    private TextView desc1, desc2 ,desc3 , desc4 ,desc5;
 
 
     @Override
@@ -31,14 +32,23 @@ public class doctors extends AppCompatActivity {
 
         doc1=(ImageView) findViewById(R.id.doctor1);
         namdoc1=(TextView) findViewById(R.id.name_doc1);
+        desc1=(TextView) findViewById(R.id.des1);
 
         doc2=(ImageView)findViewById(R.id.doctor2);
+        namdoc2=(TextView) findViewById(R.id.nam_doc2);
+        desc2=(TextView) findViewById(R.id.des2);
 
         doc3=(ImageView)findViewById(R.id.doctor3);
+        namdoc3=(TextView) findViewById(R.id.nam_doc3);
+        desc3=(TextView) findViewById(R.id.des3);
 
         doc4=(ImageView)findViewById(R.id.doctor4);
+        namdoc4=(TextView) findViewById(R.id.nam_doc4);
+        desc4=(TextView) findViewById(R.id.des4);
 
         doc5=(ImageView)findViewById(R.id.doctor5);
+        namdoc5=(TextView) findViewById(R.id.nam_doc5);
+        desc5=(TextView) findViewById(R.id.des5);
 
 
 
@@ -49,9 +59,10 @@ public class doctors extends AppCompatActivity {
 
 
 
-                android.util.Pair[] pairs = new android.util.Pair[2];
+                android.util.Pair[] pairs = new android.util.Pair[3];
                 pairs[0]= new android.util.Pair(doc1,"imagetransaction");
                 pairs[1]= new android.util.Pair(namdoc1,"nametransaction");
+                pairs[2]= new android.util.Pair(desc1,"desctransaction");
 
 
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(doctors.this , pairs);
@@ -66,9 +77,10 @@ public class doctors extends AppCompatActivity {
 
 
 
-                android.util.Pair[] pairs = new android.util.Pair[2];
+                android.util.Pair[] pairs = new android.util.Pair[3];
                 pairs[0]= new android.util.Pair(doc2,"imagetransaction2");
-                pairs[1]= new android.util.Pair(namdoc1,"nametransaction2");
+                pairs[1]= new android.util.Pair(namdoc2,"nametransaction2");
+                pairs[2]= new android.util.Pair(desc2,"desctransaction2");
 
 
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(doctors.this , pairs);
@@ -84,9 +96,10 @@ public class doctors extends AppCompatActivity {
 
 
 
-                android.util.Pair[] pairs = new android.util.Pair[2];
+                android.util.Pair[] pairs = new android.util.Pair[3];
                 pairs[0]= new android.util.Pair(doc3,"imagetransaction3");
-                pairs[1]= new android.util.Pair(namdoc1,"nametransaction3");
+                pairs[1]= new android.util.Pair(namdoc3,"nametransaction3");
+                pairs[2]= new android.util.Pair(desc3,"desctransaction3");
 
 
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(doctors.this , pairs);
@@ -102,9 +115,10 @@ public class doctors extends AppCompatActivity {
 
 
 
-                android.util.Pair[] pairs = new android.util.Pair[2];
+                android.util.Pair[] pairs = new android.util.Pair[3];
                 pairs[0]= new android.util.Pair(doc4,"imagetransaction4");
-                pairs[1]= new android.util.Pair(namdoc1,"nametransaction4");
+                pairs[1]= new android.util.Pair(namdoc4,"nametransaction4");
+                pairs[2]= new android.util.Pair(desc4,"desctransaction4");
 
 
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(doctors.this , pairs);
@@ -120,9 +134,10 @@ public class doctors extends AppCompatActivity {
 
 
 
-                android.util.Pair[] pairs = new android.util.Pair[2];
+                android.util.Pair[] pairs = new android.util.Pair[3];
                 pairs[0]= new android.util.Pair(doc5,"imagetransaction5");
-                pairs[1]= new android.util.Pair(namdoc1,"nametransaction5");
+                pairs[1]= new android.util.Pair(namdoc5,"nametransaction5");
+                pairs[2]= new android.util.Pair(desc5,"desctransaction5");
 
 
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(doctors.this , pairs);
