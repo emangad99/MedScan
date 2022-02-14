f1e0e09b0b5eb96c18086396feced50ff9b851ff
eman_gad <emangad3110@gmail.com>
2022-02-14 17:58:08 +0200

    init commit

--- app/src/main/java/com/example/medscan/Feedback.java
+++ app/src/main/java/com/example/medscan/Feedback.java
@@ -12,10 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Feedback extends AppCompatActivity {
-    String answarValue;
     Button btn_submit;
-    RatingBar ratestars;
-    TextView feedback;
     Animation charactanim;
 
     @Override
@@ -25,52 +22,8 @@ public class Feedback extends AppCompatActivity {
 
 
         btn_submit=findViewById(R.id.submit);
-        ratestars=findViewById(R.id.ratingBar);
-        feedback=findViewById(R.id.result_feedback);
         charactanim= AnimationUtils.loadAnimation(this,R.anim.charactanim);
-
         btn_submit.startAnimation(charactanim);
-
-        ratestars.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                answarValue= String.valueOf((int)(ratestars.getRating()));
-                if(answarValue.equals("1"))
-                {
-                    feedback.setText("Not Bad");
-                    btn_submit.startAnimation(charactanim);
-
-                }
-                else if (answarValue.equals("2"))
-                {
-                    feedback.setText("Good");
-                    btn_submit.startAnimation(charactanim);
-
-                }
-                else if (answarValue.equals("3"))
-                {
-                    feedback.setText("Very Good");
-                    btn_submit.startAnimation(charactanim);
-
-                }
-                else if (answarValue.equals("4"))
-                {
-                    feedback.setText("Excellent");
-                    btn_submit.startAnimation(charactanim);
-
-                }
-                else if (answarValue.equals("5"))
-                {
-                    feedback.setText("Awesome");
-                    btn_submit.startAnimation(charactanim);
-
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "No Point", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
     }
 }
