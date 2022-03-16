70f1a193d1a211dbeca02149bf2b8c191c365cab
eman_gad <emangad3110@gmail.com>
2022-03-16 20:39:08 +0200

    init commit

--- app/src/main/java/com/example/medscan/RayUploaded.java
+++ app/src/main/java/com/example/medscan/RayUploaded.java
@@ -1,14 +1,43 @@
 package com.example.medscan;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
 
 public class RayUploaded extends AppCompatActivity {
 
+    ImageView btn_choose , image;
+    Button upload ;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference("image");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ray_uploaded);
+
+        btn_choose = findViewById(R.id.choose);
+        image=findViewById(R.id.image_ray);
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+
 }
