95463bca424def67fc85f56a6077f7bab853efcc
henar <henarygaber@gmail.com>
2022-03-16 22:08:38 +0200

    henar

--- app/src/main/java/com/example/medscan/PatientEdit.java
+++ app/src/main/java/com/example/medscan/PatientEdit.java
@@ -2,17 +2,16 @@ package com.example.medscan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,26 +19,39 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class PatientEdit extends AppCompatActivity {
-TextView password;
-EditText fullname;
-    EditText email;
-    EditText phone;
+    TextView passwordt;
+    private EditText fullname;
+    private EditText emailedittext;
+    private EditText phone;
     EditText medical;
     EditText clinic;
     EditText time;
     EditText other;
-Button update;
-FirebaseAuth mAuth;
-FirebaseUser mUser;
-DatabaseReference muserRuf;
+    ImageView profileimage;
+    Button update;
+    private FirebaseDatabase database;
+    private DatabaseReference muserRuf;
+    private String email,password;
+    private static  final  String USERS ="Users";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_edit);
 
-        password = findViewById(R.id.txt_reset);
+        Intent intent = getIntent();
+         email = intent.getStringExtra("email");
+
+         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+         DatabaseReference UserRef = rootRef.child(USERS);
+
+         Log.v("EMAILADD", UserRef.orderByChild("email").equalTo(email).toString());
+
+
+
+        passwordt = findViewById(R.id.txt_reset);
         fullname = findViewById(R.id.editTextTextPersonName2);
-        email = findViewById(R.id.editTextTextEmailAddress);
+        emailedittext = findViewById(R.id.editTextTextEmailAddress);
         phone = findViewById(R.id.editTextPhone);
         medical = findViewById(R.id.editTextTextPersonName);
         clinic = findViewById(R.id.editTextTextPersonName3);
@@ -47,14 +59,34 @@ DatabaseReference muserRuf;
         other = findViewById(R.id.editTextTextPersonName5);
         update = findViewById(R.id.button3);
 
-        mAuth = FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
-        muserRuf = FirebaseDatabase.getInstance().getReference().child("Users");
+        database = FirebaseDatabase.getInstance();
+        muserRuf = database.getReference(USERS);
+
+
+        muserRuf.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds : snapshot.getChildren()){
+
+                    if(ds.child("Email").getValue().equals(email)){
+                        fullname.setText(ds.child("First Name").getValue(String.class));
+                        phone.setText(ds.child("phone").getValue(String.class));
+                        emailedittext.setText(email);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
 
-        password.setOnClickListener(new View.OnClickListener() {
+
+        passwordt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -62,43 +94,6 @@ DatabaseReference muserRuf;
                 startActivity(reset);
             }
 
-            protected void onStart(){
-                PatientEdit.super.onStart();
-                if(mUser==null){
-
-                    sendUserToLoginActivity();
-                }
-
-                else{
-                    muserRuf.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            if(snapshot.exists()){
-                                String fullname = String.valueOf(snapshot.child("First Name").getValue());
-                                String  email = String.valueOf(snapshot.child("Email").getValue());
-                                String  phone = String.valueOf(snapshot.child("Phone").getValue());
-
-
-
-
-
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(PatientEdit.this,"sorry, Something going Wrong ", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            }
-
-            private void sendUserToLoginActivity() {
-
-                Intent intent= new Intent(PatientEdit.this,Login.class);
-                startActivity(intent);
-                finish();
-            }
 
         });
 
