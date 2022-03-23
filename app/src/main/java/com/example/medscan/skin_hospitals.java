8440b01c19a06e4b7e8c67de0051ba0090b1c1b2
eman_gad <emangad3110@gmail.com>
2022-03-23 19:34:26 +0200

    init commit

--- app/src/main/java/com/example/medscan/skin_hospitals.java
+++ app/src/main/java/com/example/medscan/skin_hospitals.java
@@ -10,6 +10,8 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class skin_hospitals extends AppCompatActivity {
 
@@ -106,6 +108,15 @@ public class skin_hospitals extends AppCompatActivity {
         else {
             super.onBackPressed();
         }
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.color6));
+
+        }
     }
+
 
 }
