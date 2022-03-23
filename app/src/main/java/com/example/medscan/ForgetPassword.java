73ecad08116b51b21db969396d8c5993d585b721
eman_gad <emangad3110@gmail.com>
2022-03-23 16:19:29 +0200

    init commit

--- app/src/main/java/com/example/medscan/ForgetPassword.java
+++ app/src/main/java/com/example/medscan/ForgetPassword.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class ForgetPassword extends AppCompatActivity {
-    Button reset;
+    Button reset , auth;
     EditText oldPass , newPass,confirmPass;
+    boolean passvisible;
     String pw = "^.*(?=.{8,})(?=.*\\d)(?=.*[a-zA-Z])|(?=.{8,})(?=.*\\d)(?=.*[!@#$%^&])|(?=.{8,})(?=.*[a-zA-Z])(?=.*[!@#$%^&]).*$";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class ForgetPassword extends AppCompatActivity {
         oldPass=findViewById(R.id.txt_old_password);
         newPass=findViewById(R.id.txt_new_password);
         confirmPass=findViewById(R.id.txt_confirm_password);
+        auth=findViewById(R.id.authentication);
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +66,56 @@ public class ForgetPassword extends AppCompatActivity {
 
             }
         });
+
+        newPass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int Right = 2 ;
+                if(event.getAction()==MotionEvent.ACTION_UP) {
+                    if(event.getRawX()>= newPass.getRight()-newPass.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection = newPass.getSelectionEnd();
+                        if(passvisible) {
+                            newPass.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.visibility_off_icon,0);
+                            newPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passvisible=false;
+                        }
+                        else {
+                            newPass.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.visibility_icon,0);
+                            newPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passvisible=true;
+                        }
+                        newPass.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        confirmPass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int Right = 2 ;
+                if(event.getAction()==MotionEvent.ACTION_UP) {
+                    if(event.getRawX()>= confirmPass.getRight()-confirmPass.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection = confirmPass.getSelectionEnd();
+                        if(passvisible) {
+                            confirmPass.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.visibility_off_icon,0);
+                            confirmPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passvisible=false;
+                        }
+                        else {
+                            confirmPass.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.visibility_icon,0);
+                            confirmPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passvisible=true;
+                        }
+                        confirmPass.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
 }
