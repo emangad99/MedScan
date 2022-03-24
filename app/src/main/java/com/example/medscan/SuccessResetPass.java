6dd9ecde33f67beaad4236879b7909139532e926
eman_gad <emangad3110@gmail.com>
2022-03-24 23:46:02 +0200

    init commit

--- app/src/main/java/com/example/medscan/SuccessResetPass.java
+++ app/src/main/java/com/example/medscan/SuccessResetPass.java
@@ -9,19 +9,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class SuccessResetPass extends AppCompatActivity {
-Button login;
+    Button login;
+    SessionManager sessionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_reset_pass);
 
+        sessionManager=new SessionManager(getApplicationContext());
 
         login = findViewById(R.id.btn_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent login=new Intent(SuccessResetPass.this,Login.class);
-                startActivity(login);
+
+                sessionManager.setLogin(false);
+                sessionManager.setUsername("");
+                startActivity(new Intent(SuccessResetPass.this,Login.class));
+
             }
         });
     }
