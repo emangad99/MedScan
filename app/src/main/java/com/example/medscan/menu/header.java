f5b8f2fb2f96865cc9a7476c70c533bd9bcffd94
eman_gad <emangad3110@gmail.com>
2022-04-05 22:20:43 +0200

    init commit

--- app/src/main/java/com/example/medscan/menu/header.java
+++ app/src/main/java/com/example/medscan/menu/header.java
@@ -3,14 +3,23 @@ package com.example.medscan.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.medscan.R;
 
 public class header extends AppCompatActivity {
 
+    ImageView img;
+    TextView name,email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_header);
+
+        img=findViewById(R.id.pic);
+        name=findViewById(R.id.user_name);
+        email=findViewById(R.id.user_email);
     }
 }
